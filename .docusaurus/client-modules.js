ea4826feede896066c55d84213a8e20d9bab9455
Sahana Srinivasan <sahanas009@gmail.com>
2021-03-16 16:59:00 -0400

    Create initial docusaurus site

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/sahana/Documents/wics/wics-resources/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/sahana/Documents/wics/wics-resources/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/sahana/Documents/wics/wics-resources/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/sahana/Documents/wics/wics-resources/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/sahana/Documents/wics/wics-resources/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/sahana/Documents/wics/wics-resources/src/css/custom.css"),
+];
